ced1934e2acc5b09a98da406ee262f0b17e1dc79
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-05 17:53:07 +0100

    Re-factored.
    Cards display now takes care of initial fanned cut for deal and also pack.
    Added instructions.

--- drawer.rb
+++ drawer.rb
@@ -13,17 +13,41 @@ module Cribbage
       # 'Watermark' on the background
       @game.font[:watermark].draw( 'The Julio', WATERMARK_POS.x, WATERMARK_POS.y, 0,
                               1, 1, WATERMARK )
-                              
+
       score_box
-      
+
       draw_grid
     end
-    
+
     def cards
+      if [:initial_cut, :cpu_cut, :cut_complete].include? @game.phase
+        return @game.pack.draw_fan( FAN_POS, CARD_GAP )
+      end
+
       @game.player_hand.draw( orient: :face_up )
       @game.cpu_hand.draw
+      @game.pack.draw
     end
-    
+
+    def instructions
+      return unless @game.instructions
+
+      inst = @game.instructions
+
+      font    = @game.font[:instructions]
+      size    = font.measure inst[:text]
+      margin  = font.text_width 'X'
+
+      left = inst[:left] || [MID_X - (size.width / 2), 3].max
+      top  = inst[:top]  || INSTRUCTION_TOP
+
+      @game.draw_rectangle( Point.new( left - margin, top ),
+        Size.new( size.width + margin * 2, size.height * 2 ), 6, WATERMARK )
+
+      font.draw( inst[:text], left, top + size.height / 2, 7,
+        1, 1, Gosu::Color::WHITE )
+    end
+
     private
 
     def score_box
@@ -34,7 +58,7 @@ module Cribbage
 
       @game.draw_rectangle( box.offset( 1, 1 ), size.deflate( 2, 2 ), 0, SCORE_BKGR )
     end
-    
+
     def draw_grid
       vert  = Size.new( 1, HEIGHT )
       horiz = Size.new( WIDTH, 1 )
